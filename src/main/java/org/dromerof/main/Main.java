10cce01029245adfae672fa2d89e1204d4d7d4e0
Daniel Romero <dandres508@gmail.com>
2024-10-09 14:25:07 -0500

    creacion del archivo .env

--- src/main/java/org/dromerof/main/Main.java
+++ src/main/java/org/dromerof/main/Main.java
@@ -1,17 +1,25 @@
 package org.dromerof.main;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Cargar las variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Leer las variables de entorno
+        String dbUrl = dotenv.get("DB_URL");
+        String dbUser = dotenv.get("DB_USER");
+        String dbPassword = dotenv.get("DB_PASSWORD");
 
         Connection myConn = null;
         Statement myStant = null;
         ResultSet myRes = null;
 
         try {
-            // 1. Connexion a nuestra base de datos
-            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "dromerof");
+            // 1. Conexión a nuestra base de datos usando las variables de entorno
+            myConn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
             // 2. Crear una declaración objeto
             myStant = myConn.createStatement();
             // 3. Ejecutar consulta SQL
@@ -22,8 +30,7 @@ public class Main {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("Algo salio mal");
+            System.out.println("Algo salió mal");
         }
-
     }
 }
