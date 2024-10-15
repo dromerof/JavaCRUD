ee4f8f368e2149f7be5345f513d1c0643d09db80
Daniel Romero <dandres508@gmail.com>
2024-10-15 15:05:24 -0500

    Utilizando un pool

--- src/main/java/org/dromerof/main/Main.java
+++ src/main/java/org/dromerof/main/Main.java
@@ -3,50 +3,18 @@ package org.dromerof.main;
 import org.dromerof.model.Employee;
 import org.dromerof.repository.EmployeeRepository;
 import org.dromerof.repository.Repository;
-import org.dromerof.util.DatabaseConnection;
-//import org.dromerof.view.SwingApp;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        try (Connection myConn = DatabaseConnection.getInstance()) {
-            if (myConn.getAutoCommit()) {
+        System.out.println("------ Listando todos ------");
+        Repository<Employee> repository = new EmployeeRepository();
+        repository.findAll().forEach(System.out::println);
 
-                myConn.setAutoCommit((false));
-            }
-
-            try {
-                Repository<Employee> repository = new EmployeeRepository(myConn);
-
-                System.out.println("---------Insertar un nuevo cliente---------");
-                Employee employee = new Employee();
-
-                /*
-                employee.setFirst_name("Andres");
-                employee.setPa_surname("Romero");
-                employee.setMa_surname("Fontalvo");
-                employee.setEmail("andres@example.com");
-                employee.setSalary(20000F);
-                employee.setCurp("JHGTYUIOLKJHGDFETW");
-                repository.save(employee);
-                myConn.commit();*/
-
-                employee.setFirst_name("Dany");
-                employee.setPa_surname("Puerta");
-                employee.setMa_surname("Romero");
-                employee.setEmail("Dany@example.com");
-                employee.setSalary(30000F);
-                employee.setCurp("JHGTYUIOLKJHGDFETW");
-                repository.save(employee);
-                myConn.commit();
-
-            } catch (SQLException e) {
-                myConn.rollback();
-                throw new RuntimeException(e);
-            }
-        }
+        System.out.println("------ Buscando por ID -----");
+        System.out.println(repository.getByID(2));
 
     }
 }
