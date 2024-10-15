28631688bece9e4dcf675d868d6ecad35c7dd758
Daniel Romero <dandres508@gmail.com>
2024-10-15 14:51:55 -0500

    Implementando conexiones pool

--- src/main/java/org/dromerof/util/DatabaseConnection.java
+++ src/main/java/org/dromerof/util/DatabaseConnection.java
@@ -1,9 +1,9 @@
 package org.dromerof.util;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
@@ -13,13 +13,24 @@ public class DatabaseConnection {
     private static final String dbUser = dotenv.get("DB_USER");
     private static final String dbPassword = dotenv.get("DB_PASSWORD");
 
-    private static Connection myConn;
+    private static BasicDataSource pool;
 
-    public static Connection getInstance() throws SQLException {
-        if(myConn == null){
-            myConn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    public static BasicDataSource getInstance() {
+        if (pool == null) {
+            pool = new BasicDataSource();
+            pool.setUrl(dbUrl);
+            pool.setUsername(dbUser);
+            pool.setPassword(dbPassword);
+
+            pool.setInitialSize(3);
+            pool.setMinIdle(2);
+            pool.setMaxIdle(10);
+            pool.setMaxTotal(10);
         }
-        return myConn;
+        return pool;
     }
 
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
+    }
 }
